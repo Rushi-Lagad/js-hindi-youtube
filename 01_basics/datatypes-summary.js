2c6a5a68fdb5591bb2f17c006db7c755361c7262
Rushikesh Lagad <84847554+Rushi-Lagad@users.noreply.github.com>
2024-05-16 06:43:31 +0000

    Data Type Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+//  Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('1234')
+const anotherId = Symbol('1234')
+
+console.log(id === anotherId);
+
+const bigNumber = 123454567n;
+
+// Reference Type / Non Primitive
+
+// Array, Objects, Functions
+
+const heros = ["rushi", "mayur"];
+
+let myObj = {
+    name: "Rushi",
+    age: 28
+}
+
+const myFunction = function () {
+    console.log("Hello");
+}
+myFunction()
+
+console.log(typeof heros);
