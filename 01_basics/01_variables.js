aa5b2218c128e6d7cb850818f1fef65d134f39b2
Rushikesh Lagad <84847554+Rushi-Lagad@users.noreply.github.com>
2024-05-16 04:41:45 +0000

    let cont var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 14445;
+let accountEmail = "rushi@gmail.com";
+var accountPassword = "1234";
+accountCity = "Pune";
+let accountState;   // undefined
+
+// accountId = 234;
+accountEmail = "rushi";
+accountPassword = "234";
+accountCity = "Karjat";
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer Not to use var
+because of issue in block scope and fun scope
+
+*/
