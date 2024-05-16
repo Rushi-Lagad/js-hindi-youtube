a1c96fa55efa35207439a437e4e4907534057b28
Rushikesh Lagad <84847554+Rushi-Lagad@users.noreply.github.com>
2024-05-16 04:56:10 +0000

    Data Types & ECMA Standards

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all js code as newer version
+
+// number  => 2 to power 53
+// bigint
+// string => ""
+// boolean => true false
+// null => standalone value     let a = nulll
+// undefind =>  let a;
+// symbol => uniqueness find out
+
+// object => 
+
+    console.log(typeof "rushi");
+    console.log(typeof null);   // object
+    console.log(typeof undefined);   // undefined
+
