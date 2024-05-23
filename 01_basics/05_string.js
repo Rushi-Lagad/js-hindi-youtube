b2fc24e4047fc64ed0d2593cdd9b25ff8b2b7570
Rushikesh Lagad <84847554+Rushi-Lagad@users.noreply.github.com>
2024-05-23 13:54:13 +0000

    trim method

---
+++ 01_basics/05_string.js
@@ -0,0 +1,28 @@
+const name = "Rushi"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String("Rushi-Lagad")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('u'));
+
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 7)
+// console.log(anotherString);
+
+const newStringOne = "    rushi   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+// split, slice
